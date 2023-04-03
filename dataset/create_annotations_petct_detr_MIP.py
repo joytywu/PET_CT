75401cb35d14fc76853e328a7d88bd38e4b695b0
joytywu <joytywu@RAD-gpu.stanford.edu>
2023-04-02 17:54:02 -0700

    commit from nm machine

--- dataset/create_annotations_petct_detr_MIP.py
+++ dataset/create_annotations_petct_detr_MIP.py
@@ -518,6 +518,7 @@ if __name__ == "__main__":
 # python src/create_annotations_petct_detr_MIP.py /media/storage/Joy/datasets/NIFTI_MIP/FDG-PET-CT-Lesions/ /media/storage/Joy/datasets/DETR_MIP/FDG-PET-CT-Lesions/
 # python create_annotations_petct_detr_MIP.py /gpfs/fs0/data/stanford_data/petct/NIFTI_MIP/FDG-PET-CT-Lesions/ /gpfs/fs0/data/stanford_data/petct/DETR_MIP/FDG-PET-CT-Lesions/
 # python create_annotations_petct_detr_MIP.py /gpfs/fs0/data/stanford_data/petct/NIFTI_MIP/FDG-PET-CT-Lesions/ /gpfs/fs0/data/stanford_data/petct/DETR_MIP2/FDG-PET-CT-Lesions/
+# python src/create_annotations_petct_detr_MIP.py /media/storage/Joy/datasets/NIFTI_MIP/FDG-PET-CT-Lesions/ /media/storage/Joy/datasets/DETR_MIP2/FDG-PET-CT-Lesions/
 
 
 # Output dataset directory structure:
